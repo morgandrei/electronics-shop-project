9f82342f71bbf9fa1ecd802ebc1dcb894d285b52
Andrey <moregun@list.ru>
2023-06-21 23:02:56 +0300

    test file

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+
+@pytest.fixture
+def number():
+    return Item('qwerty', 200, 50)
+
+
+def test_calculate_total_price(number):
+    assert number.calculate_total_price() == 10000
+
+
+def test_apply_discount(number):
+    assert number.apply_discount() == 200
