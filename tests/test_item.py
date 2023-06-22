e5b7b1c73b96d12371422c890979d36114c4838a
Andrey <moregun@list.ru>
2023-06-22 22:08:43 +0300

    poetry file

--- tests/test_item.py
+++ tests/test_item.py
@@ -12,5 +12,10 @@ def test_calculate_total_price(number):
     assert number.calculate_total_price() == 10000
 
 
+
 def test_apply_discount(number):
     assert number.apply_discount() == 200
+    number.pay_rate = 0.8
+    number.apply_discount()
+    assert 100 * number.pay_rate == 80.0
+    assert number.price == 160.0
