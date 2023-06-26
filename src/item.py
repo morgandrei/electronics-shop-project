726b5eb25812698e97fce68838a6288d1dcf0ead
Andrey <moregun@list.ru>
2023-06-26 13:24:25 +0300

    домашка 2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -5,7 +8,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int):
+    def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
 
@@ -13,22 +16,54 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
-    def calculate_total_price(self):
+        self.all.append(self)
+
+    def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
 
         :return: Общая стоимость товара.
         """
-        return int(self.price * self.quantity)
+
+        return self.price * self.quantity
 
     def apply_discount(self):
         """
         Применяет установленную скидку для конкретного товара.
         """
+        self.price *= self.pay_rate
 
-        self.price = float(self.price * self.pay_rate)
-        return self.price
+    @property
+    def name(self) -> str:
+        """геттер названия товара"""
+        return self.__name
+
+    @name.setter
+    def name(self, name) -> None:
+        """сеттер на название товара, в зависимости от длины"""
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            self.__name = name[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls) -> None:
+        """класс-метод, инициализирующий экземпляры класса Item данными из файла src/items.csv"""
+        cls.all = []
+        with open('../src/items.csv', encoding='windows-1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name = row['name']
+                price = cls.string_to_number(row['price'])
+                quantity = cls.string_to_number(row['quantity'])
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(num: str) -> int:
+        """статический метод, возвращающий число из числа-строки"""
+        return int(float(num))
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -9,13 +9,33 @@ def number():
 
 
 def test_calculate_total_price(number):
+    """Тест правильности расчета общей стоимости товара"""
     assert number.calculate_total_price() == 10000
 
 
-
 def test_apply_discount(number):
-    assert number.apply_discount() == 200
+    """Тест расчета скидки"""
+    assert number.apply_discount() == None
     number.pay_rate = 0.8
     number.apply_discount()
     assert 100 * number.pay_rate == 80.0
     assert number.price == 160.0
+
+
+
+def test_string_to_number():
+    assert Item.string_to_number('17') == 17
+    assert Item.string_to_number('17.0') == 17
+    assert Item.string_to_number('-4.3') == -4
+
+
+def test_new_name_setter(number):
+    number.name = 'qwerty'
+    assert number.name == 'qwerty'
+    number.name = 'СуперСмартфон'
+    assert number.name == 'СуперСмарт'
+
+
+def test_item_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
