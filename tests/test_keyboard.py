cb9a5d5dd97666bf2043b504885f6943a58c4f57
Andrey <moregun@list.ru>
2023-07-07 13:30:06 +0300

    домашка 5

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -3,15 +3,15 @@ from src.keyboard import MixinLang, Keyboard
 
 @pytest.fixture
 def keyboard():
-    return Keyboard('Dark Project KD87A', 9600, 5)
+    return Keyboard("Dark Project KD87A", 9600, 5)
 
 def test_Keyboard___init__():
-    kb = Keyboard('Dark Project KD87A', 9600, 5)
+    kb = Keyboard("Dark Project KD87A", 9600, 5)
     assert kb.name == "Dark Project KD87A"
 
 def test_keyboard_language(keyboard):
-    assert keyboard.language == 'EN'
+    assert keyboard.language == "EN"
     keyboard.change_lang()
-    assert keyboard.language == 'RU'
+    assert keyboard.language == "RU"
     keyboard.change_lang().change_lang()
-    assert keyboard.language == 'RU'
+    assert keyboard.language == "RU"
